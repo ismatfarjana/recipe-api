52388f7db74750f64517a28b72cd6d82507c3b31
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-10-17 13:08:49 +1100

    Style

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const authRouter = require("./routes/authRoutes");
 
 const app = express();
 app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public/image"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
